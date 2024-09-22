f15dc6fe11a43983d2bf49c52bf846666d0c1550
jan951002 <jatru95@gmail.com>
2024-09-21 20:49:45 -0500

    Revert "Garden UI has been created (#3)"
    This reverts commit 867b550dd88ad773da67daa7e5850247fdcede66.

--- app/src/main/java/com/poli/botanicalassistant/MainActivity.kt
+++ app/src/main/java/com/poli/botanicalassistant/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_garden, R.id.nav_tutorial, R.id.nav_web
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_tutorial, R.id.nav_web
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
